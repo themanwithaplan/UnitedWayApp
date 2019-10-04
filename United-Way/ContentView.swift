1a647b517ce9f3851b453ee6be9129254acdd90f
Jacob J <jacobjulagay@gmail.com>
2019-10-04 15:34:12 -0700

    add files

---
+++ United-Way/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  United-Way
+//
+//  Created by Jacob Julag-ay on 10/4/19.
+//  Copyright © 2019 UnitedWay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
